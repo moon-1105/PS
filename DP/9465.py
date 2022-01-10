a0d726cf124c6bd9557800cc396c6a615216aabd
moon-1105 <moonkite1105@gmail.com>
2022-01-11 00:23:07 +0900

    n이 젤 작을 때 따로 처리해주기

--- DP/9465.py
+++ DP/9465.py
@@ -7,7 +7,7 @@ if __name__=="__main__":
         stickers, arr = [], []
         for _ in range(0,2):
             stickers.append(list(map(int, input().split())))
-            arr.append([0]*n)
+            arr.append([0]*(n+1))
         arr[0][0], arr[1][0] = stickers[0][0], stickers[1][0]
 
         for j in range(1, n):
