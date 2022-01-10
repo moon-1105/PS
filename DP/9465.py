886768f6b43ea1a071bfc547e8a490b4ae9f2354
moon-1105 <moonkite1105@gmail.com>
2022-01-11 00:16:36 +0900

    stickers

---
+++ DP/9465.py
@@ -0,0 +1,19 @@
+# 상냥이가 뗄 수 있는 스티커의 점수의 최댓값
+# 스티커 한 장을 떼면, 그 스티커와 변을 공유하는 스티커는 모두 찢어져서 사용할 수 없게 된다
+if __name__=="__main__":
+    T = int(input())
+    for _ in range(0,T):
+        n = int(input())
+        stickers, arr = [], []
+        for _ in range(0,2):
+            stickers.append(list(map(int, input().split())))
+            arr.append([0]*n)
+        arr[0][0], arr[1][0] = stickers[0][0], stickers[1][0]
+
+        for j in range(1, n):
+            for i in range(0, 2):
+                if i==0:
+                    arr[i][j] = stickers[i][j] + max(arr[i+1][j-1],arr[i+1][j-2])
+                else:
+                    arr[i][j] = stickers[i][j] + max(arr[i-1][j-1],arr[i-1][j-2])
+        print(max(arr[0][n-1],arr[1][n-1]))
