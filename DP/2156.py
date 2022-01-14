c2c8e7dd092debfc44d337e1f1cc3b675c346760
moon-1105 <moonkite1105@gmail.com>
2022-01-14 18:44:24 +0900

    포도주

--- DP/2156.py
+++ DP/2156.py
@@ -3,13 +3,20 @@ if __name__=="__main__":
     wines = []
     for _ in range(N):
         wines.append(int(input()))
-    print(wines)
     arr = [0]*(N+1)
-    arr[0],arr[1] = wines[0], wines[1]+wines[0]
+    if len(wines) < 3:
+        print(sum(wines))
+        exit(0)
+    arr[0] = wines[0]
+    arr[1] = wines[0]+wines[1]
+    arr[2] = max(wines[0]+wines[2], wines[1]+wines[2], arr[1])
+
+    for i in range(3, N):
+        arr[i] = max(arr[i-1], wines[i]+arr[i-2], wines[i]+wines[i-1]+arr[i-3])
+
     """
-    arr[i] = arr[i]+arr[i-2] 혹은 arr[i-1]+arr[i-2]
+    wines[i]+wines[i-1]+arr[i-3]
+    => arr[i-2]를 확실하게 배제하기 위해서 
     """
-    for i in range(2, N):
-        arr[i] = max(arr[i]+arr[i-2], arr[i-1] + arr[i-2])
-    print(arr)
+    print(max(arr))
     #경우의수 골고루 생각해서 다시 하기
