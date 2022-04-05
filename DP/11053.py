6b18f9ad1dcf10304960a44cb7ea640c7747ce72
moon-1105 <moonkite1105@gmail.com>
2022-04-06 00:31:18 +0900

    임시저장

--- DP/11053.py
+++ DP/11053.py
@@ -1,12 +1,9 @@
 """
 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열
-
 """
-def findSmall(n, nums, arr):
+
+def findBitonic(n, nums, arr):
     mVal = -1
-    for i in range(0, n):
-        if nums[i] < nums[n]:
-            mVal = max(mVal, arr[i])
     return mVal
 
 if __name__=="__main__":
@@ -15,21 +12,24 @@ if __name__=="__main__":
     if N == 1:
         print(1)
         exit(0)
-    arr = [0]*(N+1)
-    arr[0] = 1
+    arr = []
+    for _ in N :
+        arr.append([0] * (N + 1))
+     #첫줄은 포함/ 둘째줄은 안포함
+    arr[0][0] = 1
     if nums[0] < nums[1]:
-        arr[1] = 2
+        arr[1][0] = 2
     else:
-        arr[1] = 1
+        arr[1][1] = 1
 
     for i in range(2, N):
-        #print(arr)
-        val = findSmall(i, nums, arr)
+        print(arr)
+        val = findBitonic(i, nums, arr)
         if val == -1 :
             arr[i] = 1
         else:
             arr[i] = val+1
-    #여기를 무조건 마지막 수를 포함하는 걸로 했음..
+    #이중표로 만들어서 해당 수가 포함했을 때 / 안했을때 나눠서 가야할거같은데
     print(max(arr))
 
 
