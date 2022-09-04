3d6cb1842c688370ed339552aded78db746bd2bd
moon-1105 <moonkite1105@gmail.com>
2022-09-04 23:29:33 +0900

    현행화

--- DP/11054.py
+++ DP/11054.py
@@ -1,43 +1,9 @@
 """
 수열 A가 주어졌을 때, 그 수열의 부분 수열 중 바이토닉 수열이면서 가장 긴 수열의 길이
-1. 바이토닉 수열을 아예 구하고
-2. 그 길이를 비교
+1. 해당 수를 포함했을 때의 바이토닉 수열 길이
+2. 안포함했을때 바이토닉 길이 => 비교
+
 """
-def plus(arr, value):
-    tmp = arr.copy()
-    tmp.append(value)
-    return tmp
-
-def isBitonic(arr, num):
-    # arr 에 num을 추가했을 때, Bitonic이 아닐 경우가 있으면
-    #return False
-    tmpArr = plus(arr,num)
-    mid = tmpArr.index(max(tmpArr))
-    small = tmpArr[:mid]
-    big = tmpArr[mid:]
-    for i in range(1, len(small)):
-        if small[i - 1] > small[i]:
-            return False
-    for i in range(1, len(big)):
-        if big[i - 1] < big[i]:
-            return False
-    return True
-
-    return True
-
-def finalCheck(arr):
-    # arr이 Bitonic이 아닐 경우가 있으면
-    #return False
-    mid = arr.index(max(arr))
-    small = arr[:mid]
-    big = arr[mid:]
-    for i in range(1,len(small)):
-        if small[i-1] > small[i]:
-            return False
-    for i in range(1,len(big)):
-        if big[i-1] < big[i]:
-            return False
-    return True
 
 if __name__=="__main__":
     N = int(input())
@@ -50,28 +16,5 @@ if __name__=="__main__":
     arr = [0] * (N + 1)
     arr[0] = [nums[0]]
 
-    if nums[0] > nums[1]:
-        arr[1] = plus(arr[0], nums[1])
-    else:
-        arr[1] = [nums[1]]
 
-    for i in range(2, N):
-        # print("#", i, arr)
-        MaxArr = []
-        for j in range(0, i):
-            # 바이토닉이 맞는지는 판단하기..
-            if isBitonic(arr[j],nums[i]):
-                # 길이가 긴지
-                if len(arr[j]) > len(MaxArr):
-                    MaxArr = arr[j].copy()
-                elif len(arr[j]) == len(MaxArr):
-                    if min(arr[j]) > min(MaxArr):
-                        MaxArr = arr[j].copy()
-
-        arr[i] = plus(MaxArr, nums[i])
-
-    ans = 0
-    for i in range(0, N):
-        if finalCheck(arr[i]):
-            ans = max(len(arr[i]), ans)
     print(ans)
---
+++ 문자열처리/10808.py
@@ -0,0 +1,12 @@
+"""
+알파벳 소문자로만 이루어진 단어 S가 주어진다.
+각 알파벳이 단어에 몇 개가 포함되어 있는지 구하는 프로그램을 작성하시오.
+"""
+
+if __name__ =="__main__":
+    word = input()
+    chars = [0]*26
+    for ch in word:
+        chars[ord(ch)-97] += 1
+    for num in chars:
+        print(num, end=" ")
