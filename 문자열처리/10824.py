0be1f7123ae5c2ae577ccb138b20b470422efcc5
moon-1105 <moonkite1105@gmail.com>
2022-09-18 21:46:55 +0900

    네 수

---
+++ 문자열처리/10824.py
@@ -0,0 +1,10 @@
+
+if __name__ =="__main__":
+    nums = input()
+    nums = list(nums.split())
+    #print(nums)
+    one = int(str(nums[0])+str(nums[1]))
+    #print(one)
+    two = int(str(nums[2])+str(nums[3]))
+    #print(two)
+    print(one+two)
