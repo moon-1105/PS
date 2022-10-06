7a89713f97f2d1d23084bd324986234082965e3e
soyeon-local <moonkite1105@gmail.com>
2022-10-06 21:04:05 +0900

    save

--- 기타자료구조/1158.py
+++ 기타자료구조/1158.py
@@ -1,19 +1,20 @@
 if __name__ =="__main__":
     N, K = input().split()
     N, K = int(N),int(K)
-
     people = []
     for idx in range(1,N+1):
         people.append(idx)
     print(people)
     idx = K
     answer= []
-    while(len(answer)!=N and len(people)!=0):
-        if idx > len(people):
-            idx = idx-len(people)
-        print(idx)
-        #while(people[idx-1] in answer):
-        #    idx+=1
-        answer.append(people.pop(idx-1))
-        print(answer)
-        idx+=K
+    pos = 0
+    while(len(answer)!=N):
+        if idx >= N:
+            idx=idx-N
+        print(idx, end=" => ")
+        if idx not in answer:
+            answer.append(idx)
+            idx += K
+        else:
+            idx += 1
+    print(answer)
