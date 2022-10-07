95e0b04aa54f879a40de41fd7418ab7146edfc21
soyeon-local <moonkite1105@gmail.com>
2022-10-07 20:10:24 +0900

    시간초과..

--- 기타자료구조/1158.py
+++ 기타자료구조/1158.py
@@ -2,19 +2,22 @@ if __name__ =="__main__":
     N, K = input().split()
     N, K = int(N),int(K)
     people = []
-    for idx in range(1,N+1):
-        people.append(idx)
-    print(people)
-    idx = K
+    for idx in range(0,N):
+        people.append(idx+1)
     answer= []
-    pos = 0
+    pos, mov = 0, 1
+    flag = 0
     while(len(answer)!=N):
-        if idx >= N:
-            idx=idx-N
-        print(idx, end=" => ")
-        if idx not in answer:
-            answer.append(idx)
-            idx += K
-        else:
-            idx += 1
+        if pos >= N:
+            pos = 0
+        if mov == K and people[pos] not in answer:
+            #print(people[pos],"add")
+            answer.append(people[pos])
+            #print(answer)
+            mov = 1
+        if people[pos] not in answer:
+            mov+=1
+        pos+=1
+
     print(answer)
+    #<3, 6, 2, 7, 5, 1, 4>
